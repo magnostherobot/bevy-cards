16a423048e6d3f61ce45cdec5e81df36a888c5c9
Tom Harley <th@kpv-lab.co.uk>
2023-08-05 23:07:08 +0100

    Flip through hearts

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,60 @@
+use std::ops::RangeInclusive;
+
 use bevy::prelude::*;
 
-#[derive(Component)]
-struct Person;
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
 
-#[derive(Component)]
-struct Name(String);
+#[derive(Component, Deref, DerefMut)]
+struct AnimationFrames(RangeInclusive<usize>);
 
-#[derive(Component)]
-enum Direction {
-    Up,
-    Down,
-}
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let texture = asset_server.load("cards.png");
+    let texture_atlas = TextureAtlas::from_grid(texture, Vec2::new(34., 48.), 13, 5, None, None);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    let indices = 0..=12usize;
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
     commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("spaghet.png"),
-            transform: Transform::from_xyz(100., 0., 0.),
+        SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            sprite: TextureAtlasSprite::new(*indices.start()),
+            transform: Transform::from_scale(Vec3::splat(6.0)),
             ..default()
         },
-        Direction::Up,
+        AnimationFrames(indices),
+        AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
     ));
 }
 
-fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
-    for (mut logo, mut transform) in &mut sprite_position {
-        match *logo {
-            Direction::Up => transform.translation.y += 150. * time.delta_seconds(),
-            Direction::Down => transform.translation.y -= 150. * time.delta_seconds(),
-        }
-
-        if transform.translation.y > 200. {
-            *logo = Direction::Down;
-        } else if transform.translation.y < -200. {
-            *logo = Direction::Up;
+fn animate(
+    time: Res<Time>,
+    mut query: Query<(
+        &AnimationFrames,
+        &mut AnimationTimer,
+        &mut TextureAtlasSprite,
+    )>,
+) {
+    for (indices, mut timer, mut sprite) in &mut query {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            sprite.index = if sprite.index == *indices.end() {
+                *indices.start()
+            } else {
+                sprite.index + 1
+            };
         }
     }
 }
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_systems(Startup, setup)
-        .add_systems(Update, sprite_movement)
+        .add_systems(Update, animate)
         .run();
 }
